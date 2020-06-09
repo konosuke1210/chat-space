761ffb71fc4780b6e1b0d0d0dd7c8be7d1d2c799
konosuke1210 <konosuke1210>
2020-06-09 20:03:53 +0900

    fix commentout

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -63,26 +63,26 @@ $('#new_message').on('submit', function(e){
 });
 })
   var reloadMessages = function() {
-  //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+  
     var last_message_id = $('.chat-main__message-list__message:last').data('message-id');
     $.ajax({
-    //ルーティングで設定した通りのURLを指定
+    
       url: "api/messages",
-    //ルーティングで設定した通りhttpメソッドをgetに指定
+    
       type: 'get',
       dataType: 'json',
-    //dataオプションでリクエストに値を含める
+    
       data: {id: last_message_id}
     })
     .done(function(messages) {
       if (messages.length !== 0) {
-    //追加するHTMLの入れ物を作る
+    
         var insertHTML = '';
-    //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+    
         $.each(messages, function(i, message) {
           insertHTML += buildHTML(message)
       });
-    //メッセージが入ったHTMLに、入れ物ごと追加
+    
       $('.chat-main__message-list').append(insertHTML);
       $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
     }
@@ -91,7 +91,6 @@ $('#new_message').on('submit', function(e){
     alert('error');
   });
 };
-//$(function(){});の閉じタグの直上(処理の最後)に以下のように追記
 if (document.location.href.match(/\/groups\/\d+\/messages/)) {
   setInterval(reloadMessages, 7000);
 }
